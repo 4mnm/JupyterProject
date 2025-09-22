7818002f4483768f21291d0196dd82531d22b3e9
USER <mbathamercyline4@gmail.com>
2025-09-22 14:07:42 +0300

    number.py

---
+++ arrays.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+A = np.array([[1, 2],
+              [3, 4]])
+
+B = np.array([[5, 6],
+              [7, 8]])
+
+
+add_result = A + B
+
+
+mul_elementwise = A * B
+
+
+mat_product = A @ B   # or np.matmul(A, B)
+
+
+print("A + B =\n", add_result)
+print("\nElementwise multiplication (A * B) =\n", mul_elementwise)
+print("\nMatrix product (A @ B) =\n", mat_product)
--- circle.py
+++ circle.py
@@ -2,26 +2,26 @@ import sys
 
 class Circle:
     units = 'cm'
-    
+
     def _init_(self, radius, position=(0, 0)):
          self.radius = radius
          self.position = position
-         
-         
+
+
     def _str_(self):
         return f"I am a circle of size {self.radius}{self.units} located at {self.position}."
-    
-    
-    def main():
+
+
+def main():
         # instantiate
         circle = Circle(radius=5)
         print(circle)
         circle.position = (30, -50)
         print(circle)
         circle.radius = 17
-        print(circle)
-        
+        print(str(circle))
+
         return 0;
-    
-    if _name_ == "_main_":
+
+if _name_ == "__main__":
         sys.exit(main())
